0b25703a071cb2590c8dc2f13e5c8bdafee4e4aa
pyk <gm@pyk.sh>
2023-12-12 06:37:35 +0700

    chore: add default rpc_url (#21)
    * chore: add default rpc_url
    
    * chore: update docs

--- milapy/_chain.py
+++ milapy/_chain.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 import httpx
 
+# from pydantic import BaseModel
+
 
 def get_ttl_hash(ms: int = 5_000) -> int:
     return round((time.time() * 1000) / ms)
@@ -11,7 +13,7 @@ def get_ttl_hash(ms: int = 5_000) -> int:
 
 @lru_cache()
 def get_block_number(
-    http_client: httpx.Client, ttl_hash: Optional[int] = None
+    http_client: httpx.Client, rpc_url: str, ttl_hash: Optional[int] = None
 ) -> int:
     payload = {
         "method": "eth_blockNumber",
@@ -19,17 +21,28 @@ def get_block_number(
         "id": 1,
         "jsonrpc": "2.0",
     }
-    r = http_client.post("https://rpc.ankr.com/eth", json=payload)
+    r = http_client.post(rpc_url, json=payload)
     json = r.json()
     return int(json["result"], 16)
 
 
 class Chain:
-    def __init__(self, id: int):
+    def __init__(self, id: int, rpc_url: Optional[str] = None):
         self.id = id
         self.http_client = httpx.Client()
+        self.rpc_url = rpc_url or self.get_default_rpc_url(id)
+
+    def get_default_rpc_url(self, id: int) -> str:
+        if id == 1:
+            return "https://cloudflare-eth.com"
+        else:
+            raise Exception(
+                f"default rpc_url for chain_id={id} is not specified"
+            )
 
     def get_block_number(self, cache_ttl: int = 5_000) -> int:
         return get_block_number(
-            http_client=self.http_client, ttl_hash=get_ttl_hash(ms=cache_ttl)
+            http_client=self.http_client,
+            rpc_url=self.rpc_url,
+            ttl_hash=get_ttl_hash(ms=cache_ttl),
         )
--- tests/test_chain.py
+++ tests/test_chain.py
@@ -1,3 +1,5 @@
+import pytest
+
 from milapy import Chain
 
 
@@ -6,6 +8,23 @@ def test_chain_id():
     assert mainnet.id == 1
 
 
+def test_chain_default_rpc_url_error():
+    with pytest.raises(Exception) as exception:
+        Chain(id=9999999)
+    assert (
+        str(exception.value)
+        == "default rpc_url for chain_id=9999999 is not specified"
+    )
+
+
+def test_chain_default_rpc_url():
+    angle = Chain(id=9999999, rpc_url="https://rpc.pyk.sh")
+    assert angle.id == 9999999
+    assert angle.rpc_url == "https://rpc.pyk.sh"
+
+
 def test_get_block_number():
+    mainnet = Chain(id=1)
+    assert mainnet.get_block_number() > 18_000_000
     mainnet = Chain(id=1)
     assert mainnet.get_block_number() > 18_000_000
